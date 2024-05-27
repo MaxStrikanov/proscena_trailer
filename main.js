cba2535f7194c889093a4359a5e35bd2d1a53589
MaxStrikanov <maxvell.88@mail.ru>
2024-05-27 16:56:26 +0300

    Мобильная версия

---
+++ main.js
@@ -0,0 +1,14 @@
+import './style.scss'
+
+import accordion from './public/components/accordion';
+import tabs from './public/components/tabs';
+import modal from './public/components/modal';
+
+document.addEventListener("DOMContentLoaded", function() {
+  accordion()
+  modal()
+  tabs()
+});
+
+
+
